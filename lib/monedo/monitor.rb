987c6bd9ca5bbe5e3121e5e5ad406caca249d2f5
Dan Thompson <dwthompson@gmail.com>
2013-02-16 14:45:55 -0500

    update monitor to display meaningful data

--- lib/monedo/monitor.rb
+++ lib/monedo/monitor.rb
@@ -19,25 +19,55 @@ module Monedo
 
       @input.each do |line|
 
-        case
-        when !!(line =~ /Address:/)
-          message.clear
-          message.merge!(address: line)
-        when !!(line =~ /Numeric:/)
-          message.merge!(numeric: line)
-        when !!(line =~ /Alpha:/)
-          message.merge!(alpha: line)
-        end
+        compose(message, line)
 
-        if [:address, :numeric, :alpha].all? { |k| message.key?(k) }
-          @queue << message
-          @output.puts message[:address],
-                       message[:numeric],
-                       message[:alpha]
+        if composed?(message)
+          @queue << message.clone
+          display(message)
           message.clear
         end
 
       end
+    end
+
+    def display(message)
+      @output.puts message[:address],
+                   message[:numeric],
+                   message[:alpha]
+    end
+
+    def compose(message, line)
+      case
+      when !!(line =~ /Address:/)
+        message.clear
+        message.merge! pluck_address_data(line)
+      when !!(line =~ /Numeric:/)
+        message.merge! pluck_numeric_data(line)
+      when !!(line =~ /Alpha:/)
+        message.merge! pluck_alpha_data(line)
+      end
+    end
+
+    def composed?(message)
+      [:address, :numeric, :alpha].all? { |k| message.key?(k) }
+    end
+
+    def pluck_address_data(line)
+      pluck_data(:address, '(?<=address:\s\s\s\s)(\d+)', line)
+    end
+
+    def pluck_numeric_data(line)
+      pluck_data(:numeric, '(?<=numeric:\s)(.+)', line)
+    end
+
+    def pluck_alpha_data(line)
+      pluck_data(:alpha, '(?<=alpha:\s)(.+)', line)
+    end
+
+    def pluck_data(key, pattern, line)
+      data = line[/#{pattern}/i]
+
+      (data.nil? || data.strip.empty?) ? {} : { key => data.strip }
     end
   end
 
--- spec/monedo/monitor_spec.rb
+++ spec/monedo/monitor_spec.rb
@@ -3,13 +3,16 @@ require 'spec_helper'
 describe Monedo::Monitor do
 
   describe '#run' do
-    let(:address_part) { "POCSAG1200-: Address:    2222  Function: 0" }
-    let(:numeric_part) { "POCSAG1200-: Numeric: 2 22-2[2.2U222  2U22 ]]2UU222U22" }
-    let(:alpha_part) { "POCSAG1200-: Alpha: Message for 2222 <EOT>" }
-    let(:message) { [ address_part, numeric_part, alpha_part ] }
+    let(:address) { '2222' }
+    let(:numeric) { '2 22-2[2.2U222  2U22 ]]2UU222U22' }
+    let(:alpha) { 'Message for 2222 <EOT>' }
+    let(:address_part) { "POCSAG1200-: Address:    #{address}  Function: 0" }
+    let(:numeric_part) { "POCSAG1200-: Numeric: #{numeric}" }
+    let(:alpha_part) { "POCSAG1200-: Alpha: #{alpha}" }
+    let(:message_parts) { [ address_part, numeric_part, alpha_part ] }
 
     it 'adds a message composed of parts to the queue' do
-      input = message
+      input = message_parts
       monitor = Monedo::Monitor.new(input, StringIO.new)
       monitor.run
 
@@ -20,7 +23,7 @@ describe Monedo::Monitor do
       it 'ignores non-sequential parts' do
         bad_address_part = "POCSAG1200-: Address:    9999  Function: 0"
         bad_alpha_part = "POCSAG1200-: Alpha: 999<ESC><RS>H,<VT><FF><CR>(1s4K<"
-        input = [bad_address_part, message, bad_alpha_part].flatten
+        input = [bad_address_part, message_parts, bad_alpha_part].flatten
         monitor = Monedo::Monitor.new(input, StringIO.new)
         monitor.run
 
@@ -33,6 +36,17 @@ describe Monedo::Monitor do
         monitor.run
 
         expect(monitor.queue.length).to eq(0)
+      end
+
+      it 'extracts essential information from the parts' do
+        input = message_parts
+        monitor = Monedo::Monitor.new(input, StringIO.new)
+        monitor.run
+
+        message = monitor.queue.first
+        expect(message[:address]).to eq(address)
+        expect(message[:numeric]).to eq(numeric)
+        expect(message[:alpha]).to eq(alpha)
       end
     end
   end
